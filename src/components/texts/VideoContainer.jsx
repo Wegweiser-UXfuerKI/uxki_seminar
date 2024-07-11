0dc16bf5340c1e2f2522a137075a6882a8580d59
Lukas-Rdt <danaphor@gmail.com>
2024-07-11 12:59:54 +0200

    fix video height

--- src/components/texts/VideoContainer.jsx
+++ src/components/texts/VideoContainer.jsx
@@ -1,14 +1,32 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 export const VideoContainer = ({ link, title }) => {
+  const [videoHeight, setVideoHeight] = useState(500);
+  const videoRef = useRef(0);
+
+  useEffect(() => {
+    const calcHeight = () => {
+      if (videoRef.current) {
+        const width = videoRef.current.offsetWidth;
+        const height = (width / 16) * 9;
+        setVideoHeight(height);
+      }
+    };
+    calcHeight();
+    window.addEventListener("resize", calcHeight);
+    return () => {
+      window.removeEventListener("resize", calcHeight);
+    };
+  }, []);
+
   return (
     <iframe
       src={link}
       title={title}
+      ref={videoRef}
       width="100%"
-      height="450px"
+      height={videoHeight}
       className="rounded-xl"
-      allowFullScreen
-    ></iframe>
+      allowFullScreen></iframe>
   );
 };

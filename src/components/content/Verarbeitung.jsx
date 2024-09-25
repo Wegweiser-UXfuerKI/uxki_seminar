6d26315f94bd2cf037bb7403be7f826e1d2a1654
Lukas-Rdt <danaphor@gmail.com>
2024-09-25 17:59:26 +0200

    added files for other modules

---
+++ src/components/content/Verarbeitung.jsx
@@ -0,0 +1,10 @@
+import imageElement from "../../assets/svg/ki_placeholder.svg";
+
+const Verarbeitung = {
+  linkName: "verarbeitung",
+  name: "Verarbeitung",
+  moduleImage: imageElement,
+  content: [],
+};
+
+export default Verarbeitung;

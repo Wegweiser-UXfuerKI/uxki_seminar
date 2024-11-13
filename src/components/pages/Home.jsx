98a7f2b01c60309b02886ab6795b5d0ad78cb4b9
Lukas-Rdt <danaphor@gmail.com>
2024-11-13 17:30:36 +0100

    add disabled state

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -10,6 +10,15 @@ import UXButton from "../UXButton";
 import { AppContext } from "../../AppContext";
 
 const Home = () => {
+  // tmp solution to show unfinished modules
+  const disabledModules = [
+    "ux-bezogene-ki-eigenschaften",
+    "dateninput",
+    "verarbeitung",
+    "ki-ergebnisse",
+    "identifikation-prozesse",
+  ];
+
   const modulesData = getModuleLinksAndNames();
   const { scrollToChapter, setScrollToChapter } = useContext(AppContext);
   const moduleRefs = useRef([]);
@@ -33,56 +42,68 @@ const Home = () => {
   return (
     <div className="w-full flex flex-col justify-center items-center my-20">
       {modulesData &&
-        modulesData.map((module, index) => (
-          <div
-            className={`${styles.moduleCard} w-10/12 rounded-3xl h-fit min-h-[400px]`}
-            key={index}
-            ref={(el) => (moduleRefs.current[index] = el)}>
-            <Link
-              to={`/${module[0]}`}
-              className="w-full flex flex-grow justify-between p-14">
-              <div className="relative flex flex-col-reverse h-full w-1/3">
-                <div className="absolute top-0 left-0 z-10">
-                  <img
-                    src={getModuleImageByLink(module[0])}
-                    alt={`module icon: ${module[0]}`}
-                    className="w-auto max-h-[150px] max-w-[150px] object-contain fill-ux_lightgrey"
-                  />
+        modulesData.map((module, index) => {
+          const isDisabled = disabledModules.includes(module[0]);
+
+          return (
+            <div
+              className={`${
+                styles.moduleCard
+              } w-10/12 rounded-3xl h-fit min-h-[400px] ${
+                isDisabled ? "opacity-50 pointer-events-none" : ""
+              }`}
+              key={index}
+              ref={(el) => (moduleRefs.current[index] = el)}>
+              <Link
+                to={`/${module[0]}`}
+                className="w-full flex flex-grow justify-between p-14"
+                onClick={(e) => isDisabled && e.preventDefault()}>
+                <div className="relative flex flex-col-reverse h-full w-1/3">
+                  <div className="absolute top-0 left-0 z-10">
+                    <img
+                      src={getModuleImageByLink(module[0])}
+                      alt={`module icon: ${module[0]}`}
+                      className="w-auto max-h-[150px] max-w-[150px] object-contain fill-ux_lightgrey"
+                    />
+                  </div>
+                  <div className="h-1/3 w-full flex flex-col-reverse">
+                    <h2 className="text-ux_dark">{module[1]}</h2>
+                  </div>
                 </div>
-                <div className="h-1/3 w-full flex flex-col-reverse">
-                  <h2 className="text-ux_dark">{module[1]}</h2>
+                <div className="h-full w-2/3 flex flex-col items-center justify-center gap-5">
+                  {getSubtopicLinksAndNamesByModulelink(module[0])
+                    .reduce((result, value, index, array) => {
+                      if (index % 2 === 0)
+                        result.push(array.slice(index, index + 2));
+                      return result;
+                    }, [])
+                    .map((pair, pairIndex) => (
+                      <div
+                        key={pairIndex}
+                        className="w-full flex gap-5 items-center">
+                        {pair.map(([subLink, subName], subIndex) => (
+                          <Link
+                            key={subIndex}
+                            to={isDisabled ? "#" : `/${module[0]}/${subLink}`}
+                            className="h-fit w-[48%]"
+                            onClick={(e) => isDisabled && e.preventDefault()}>
+                            <UXButton
+                              text={`${
+                                pairIndex * 2 + subIndex + 1
+                              }: ${subName}`}
+                              bgColor="--lightgrey"
+                              textColor="--darkgrey"
+                              arrowColor="--darkgrey"
+                            />
+                          </Link>
+                        ))}
+                      </div>
+                    ))}
                 </div>
-              </div>
-              <div className="h-full w-2/3 flex flex-col items-center justify-center gap-5">
-                {getSubtopicLinksAndNamesByModulelink(module[0])
-                  .reduce((result, value, index, array) => {
-                    if (index % 2 === 0)
-                      result.push(array.slice(index, index + 2));
-                    return result;
-                  }, [])
-                  .map((pair, pairIndex) => (
-                    <div
-                      key={pairIndex}
-                      className="w-full flex gap-5 items-center">
-                      {pair.map(([subLink, subName], subIndex) => (
-                        <Link
-                          key={subIndex}
-                          to={`/${module[0]}/${subLink}`}
-                          className="h-fit w-[48%]">
-                          <UXButton
-                            text={`${pairIndex * 2 + subIndex + 1}: ${subName}`}
-                            bgColor="--lightgrey"
-                            textColor="--darkgrey"
-                            arrowColor="--darkgrey"
-                          />
-                        </Link>
-                      ))}
-                    </div>
-                  ))}
-              </div>
-            </Link>
-          </div>
-        ))}
+              </Link>
+            </div>
+          );
+        })}
     </div>
   );
 };

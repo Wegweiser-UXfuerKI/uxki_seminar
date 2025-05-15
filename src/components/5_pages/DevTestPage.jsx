56d5920cddcba95fad95fe4be13d9c42903f961f
Lukas-Rdt <danaphor@gmail.com>
2025-05-15 13:54:52 +0200

    added questions for chapters

--- src/components/5_pages/DevTestPage.jsx
+++ src/components/5_pages/DevTestPage.jsx
@@ -1,38 +1,42 @@
 import React from "react";
 import BasicQuiz from "../interactive/BasicQuiz";
-
-const sampleQuizData = [
-  {
-    id: 1,
-    questionText: "Was ist die Hauptstadt von Frankreich?",
-    options: ["Berlin", "Madrid", "Paris", "Rom"],
-    correctAnswer: [2],
-  },
-  {
-    id: 2,
-    questionText: "Welche der folgenden sind Programmiersprachen?",
-    options: ["HTML", "JavaScript", "CSS", "Python", "Photoshop"],
-    correctAnswer: [1, 3],
-  },
-  {
-    id: 3,
-    questionText:
-      "Wie viele Planeten hat unser Sonnensystem (offiziell anerkannt)?",
-    options: ["7", "8", "9", "10"],
-    correctAnswer: [1],
-  },
-  {
-    id: 4,
-    questionText: "Wähle alle Primzahlen aus:",
-    options: ["2", "4", "5", "7", "9", "11"],
-    correctAnswer: [0, 2, 3, 5],
-  },
-];
+// KI-bezogene UX
+import autonomieData from "../SimpleQuizContent/ki-bezogene-ux/autonomie.json";
+import situationsbewusstseinData from "../SimpleQuizContent/ki-bezogene-ux/situationsbewusstsein.json";
+import mentaleBelastungData from "../SimpleQuizContent/ki-bezogene-ux/mentale-belastung.json";
+import vertrauenswuerdigkeitData from "../SimpleQuizContent/ki-bezogene-ux/vertrauenswuerdigkeit.json";
+import diagnostizitaetData from "../SimpleQuizContent/ki-bezogene-ux/diagnostizitaet.json";
+// AI Act
+import einleitungData from "../SimpleQuizContent/ai-act/einleitung.json";
+import risikostufenBeispieleData from "../SimpleQuizContent/ai-act/risikostufen-beispiele.json";
+import risikostufenAuswirkungenData from "../SimpleQuizContent/ai-act/risikostufen-auswirkungen.json";
+import hlegData from "../SimpleQuizContent/ai-act/hleg.json";
 
 const DevTestPage = () => {
   return (
     <div className="w-full h-full">
-      <BasicQuiz quizData={sampleQuizData} />
+      <h1 className="w-full text-center">KI-bezogene UX</h1>
+      <h2 className="w-full text-center">Wahrgenommene Autonomie</h2>
+      <BasicQuiz quizData={autonomieData} />
+      <h2 className="w-full text-center">
+        Wahrgenommenes Situationsbewusstsein
+      </h2>
+      <BasicQuiz quizData={situationsbewusstseinData} />
+      <h2 className="w-full text-center">Wahrgenommene Mentale Belastung</h2>
+      <BasicQuiz quizData={mentaleBelastungData} />
+      <h2 className="w-full text-center">Wahrgenommene Vertrauenswürdigkeit</h2>
+      <BasicQuiz quizData={vertrauenswuerdigkeitData} />
+      <h2 className="w-full text-center">Wahrgenommene Diagnostizität</h2>
+      <BasicQuiz quizData={diagnostizitaetData} />
+      <h1 className="w-full text-center">EU AI-Act</h1>
+      <h2 className="w-full text-center">Einleitung</h2>
+      <BasicQuiz quizData={einleitungData} />
+      <h2 className="w-full text-center">Risikostufen - Anwendungsbeispiele</h2>
+      <BasicQuiz quizData={risikostufenBeispieleData} />
+      <h2 className="w-full text-center">Risikostufen - Auswirkungen</h2>
+      <BasicQuiz quizData={risikostufenAuswirkungenData} />
+      <h2 className="w-full text-center">HLEG</h2>
+      <BasicQuiz quizData={hlegData} />
     </div>
   );
 };
